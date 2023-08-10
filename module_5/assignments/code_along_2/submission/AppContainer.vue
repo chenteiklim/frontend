1c4020f23ad3d06865dc67b6b94365d126b9b27b
chenteiklim <83565306+chenteiklim@users.noreply.github.com>
2023-08-10 23:27:44 +0800

    Add files via upload

---
+++ module_5/assignments/code_along_2/submission/AppContainer.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import ProductForm from '@/components/ProductForm.vue'
+    import { ref, provide } from 'vue';
+    function receiveEmitAgain(value){
+        console.log(value)  
+    }
+    const inputStyle = ref({
+      color: 'red',
+      background: 'blue',
+      padding: '10px 20px'
+    });
+    provide('inputStyle', inputStyle);
+
+</script>
+<template>
+    <ProductForm @message="receiveEmitAgain"/>
+</template>
+
+
+
---
+++ module_5/assignments/code_along_2/submission/ProductInput.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <input :style="inputStyle" type="text" v-model="message">
+      <button :style="inputStyle" @click="emits('message', message)" >Emit Data</button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { inject } from 'vue';
+  import { ref} from 'vue'
+  const inputStyle = inject('inputStyle')
+  const emits = defineEmits()
+  const message = ref('')
+  </script>
